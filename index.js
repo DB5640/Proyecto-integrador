2b9da31c4994ecfcfad089ddafbff2b1da9ab297
Julian Ruiz Medina <jurumello0009@gmail.com>
2025-05-05 18:03:39 -0500

    Botones con Alert

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ document.addEventListener('DOMContentLoaded', (param1, parm2) => {
         window.location = '/inicio.html';
     })
 })
+
+document.addEventListener('DOMContentLoaded', (param3, param4) => {
+    const btnConsultar = document.querySelector(".botones #consultar")
+    btnConsultar.addEventListener('click', () => {
+        console.log('le di click a consultar')
+        alert('El usuario ha sido verificado existosamente');
+        window.location = '/inicio.html';
+    })
+})
+
+document.addEventListener('DOMContentLoaded', (param5, param6) => {
+    const btnEliminar = document.querySelector(".botones #eliminar")
+    btnEliminar.addEventListener('click', () => {
+        console.log('le di click a eliminar')
+        alert('Tu cuenta ha sido eliminada');
+        window.location = '/inicio.html';
+    })
+})
+
+document.addEventListener('DOMContentLoaded', (param7, param8) => {
+    const btnEditar = document.querySelector(".botones #editar")
+    btnEditar.addEventListener('click', () => {
+        console.log('le di click a editar')
+        alert('Ahora puedes editar tu cuenta');
+        window.location = 'file:///C:/Users/Coopecredito/Desktop/Programar/Proyecto%20integrador/Proyecto-integrador/inicio.html';
+    })
+})
