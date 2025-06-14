546be85049120887b382fbd39fbce2908338d624
DB5640 <122465471+DB5640@users.noreply.github.com>
2025-06-14 17:03:29 -0500

    Add files via upload

---
+++ Procedimiento.sql
@@ -0,0 +1,47 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE consulta_cliente 
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	select 
+    Cl.nombre as cliente,
+    Cu.numero_cuenta,
+    Cob.estado_cobranza,
+    E.nombre as empleado_asignado,
+    E.cargo,
+    CE.fecha_asignacion
+from 
+    Cobranza Cob
+join
+    Cuenta Cu on Cob.id_cuenta = Cu.id_cuenta
+join 
+    Cliente Cl on Cu.id_cliente = Cl.id
+JOIN 
+    Cobranza_Empleado CE on Cob.id_cobranza = CE.id_cobranza_empleado
+join 
+    Empleado E on CE.id_empleado = E.id_empleado
+where
+    Cob.estado_cobranza = 'pendiente'
+    and CE.estado_asignacion = 'activo';
+
+END
+GO
